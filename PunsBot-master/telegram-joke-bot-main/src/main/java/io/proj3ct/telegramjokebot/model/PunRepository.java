64d699379a22d4324ae552043b6d5a415f7964dd
MaSorte <161929933+MaSorte@users.noreply.github.com>
2024-05-30 15:53:16 +0300

    Update PunRepository.java

--- PunsBot-master/telegram-joke-bot-main/src/main/java/io/proj3ct/telegramjokebot/model/PunRepository.java
+++ PunsBot-master/telegram-joke-bot-main/src/main/java/io/proj3ct/telegramjokebot/model/PunRepository.java
@@ -1,12 +1,13 @@
 package io.proj3ct.telegramjokebot.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface PunRepository extends JpaRepository<PuriPuns, Long> {
 
-    List<PuriPuns> findAllByIdGreaterThan(Long punId);
+    @Query("SELECT p FROM PuriPuns p ORDER BY p.rating DESC LIMIT 5")
+    List<PuriPuns> Top5ByRating();
 }
-
